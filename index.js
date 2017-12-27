d9b6467e92856467ab82a122bd5bda4fa375e904
stacey <staceyamarks@gmail.com>
2017-12-27 21:42:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -27,5 +27,9 @@ function prependKitten(name){
 }
 
 function removeLastKitten(){
-  return kittens.slice(-1)
+  return kittens.slice(0,kittens.length-1)
+}
+
+removeFirstKitten(){
+  return kittens.slice(1)
 }
